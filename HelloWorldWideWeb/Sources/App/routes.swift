bd695ce5339a3854c0dadafc76472b15d1849b9c
jporubci <97643734+jporubci@users.noreply.github.com>
2024-10-10 21:52:09 -0400

    Prologue

--- HelloWorldWideWeb/Sources/App/routes.swift
+++ HelloWorldWideWeb/Sources/App/routes.swift
@@ -1,11 +1,29 @@
 import Vapor
 
+struct Character: Content {
+    var characterName: String
+    var characterGender: String
+    var fightingSpecialty: String
+    var armorClass: String
+    var homePlanet: String
+}
+
 func routes(_ app: Application) throws {
     app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+        try await req.view.render("index", [
+            "title": "Prologue: Enter Prima Noxa"
+        ])
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.post("create-character") { req async throws -> View in
+        let character = try req.content.decode(Character.self)
+        return try await req.view.render("prologue", [
+            "title": "Prologue: Enter Prima Noxa",
+            "characterName": character.characterName,
+            "characterGender": character.characterGender,
+            "fightingSpecialty": character.fightingSpecialty,
+            "armorClass": character.armorClass,
+            "homePlanet": character.homePlanet
+        ])
     }
 }
